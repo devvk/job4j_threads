99baa799a50dcb786d0e77b0a18574b0068453b4
devvk <info@sekretov.net>
2026-03-19 15:48:43 +0100

    change pom.xml

--- src/main/java/org/example/DeleteMe.java
+++ src/main/java/org/example/DeleteMe.java
@@ -1,7 +1,7 @@
 package org.example;
 
-public class Main {
+public class DeleteMe {
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("Hello World");
     }
 }
