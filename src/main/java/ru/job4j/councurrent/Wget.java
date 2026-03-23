6114bd58d647bc4288816f90fa4369521792a661
devvk <info@sekretov.net>
2026-03-23 12:30:30 +0100

    4. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/councurrent/Wget.java
@@ -0,0 +1,17 @@
+package ru.job4j.councurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.print("\rLoading: " + i + "%");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        thread.start();
+    }
+}
